21b1a782b8d915cc62d69e209afd24bfd32661b0
Elias C <eliascole3@gmail.com>
2015-09-25 14:21:55 -0700

    testing live edit

--- LaravelTesting/resources/views/welcome.blade.php
+++ LaravelTesting/resources/views/welcome.blade.php
@@ -39,6 +39,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5</div>
+                <div style='font-size:30px;'>Much Happy. Such Develop. Wow.</div>
             </div>
         </div>
     </body>
