136bed2121f144fbd47165094ce234181baa5736
Elias C <eliascole3@gmail.com>
2015-10-03 11:54:11 -0700

    Updated wrapper template, nav is working, style is close to the original

--- LaravelTesting/resources/views/misc/resume.blade.php
+++ LaravelTesting/resources/views/misc/resume.blade.php
@@ -1,7 +1,11 @@
-<div class="download" >
-    <a href="../files/EliasColeResume.docx">Download as a docx</a>
-</div>
-    
-<div id="iframediv">    
-    <iframe class="iframe" name="window" frameborder=2 width=900 height=1100 src="../files/EliasColeResumePDF.pdf"></iframe>
-</div>
+@extends('wrapper')
+ 
+@section('content')
+    <div class="download" >
+        <a href="../files/EliasColeResume.docx">Download as a docx</a>
+    </div>
+        
+    <div id="iframediv">    
+        <iframe class="iframe" name="window" frameborder=2 width=900 height=1100 src="../files/EliasColeResumePDF.pdf"></iframe>
+    </div>
+@endsection
--- LaravelTesting/resources/views/wrapper.blade.php
+++ LaravelTesting/resources/views/wrapper.blade.php
@@ -2,16 +2,47 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>{{ isset($title) ? $title . ' - ' : null }}Laravel - The PHP Framework For Web Artisans</title>
+	<title>Elias Cole III</title>
     {!! HTML::style('css/app.css') !!}
     {!! HTML::style('css/css.css') !!}
+    {!! HTML::script('js/nav.js') !!}
 </head>
 <body>
 
+    <div id='wrapper'>
+        <div id='body'>
 
-	<div class="content">
-        @yield('content')
+
+                <div id='header'>
+                    <b>Elias Cole III</b>
+                </div>
+
+                <div id="menu2">
+                    <ul id="sddm">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/resume">Resume</a></li>
+                        
+                        <li><a href="/portfolio" onmouseover="mopen('div1')" onmouseout="mclosetime()">Portfolio</a>
+                            <div id="div1" onmouseover="mcancelclosetime()" onmouseout="mclosetime()">
+                                <a href='/portfolio/assembly'> Assembly MASM </a> 
+                                <a href='/portfolio/matching-game'> Matching Game </a> 
+                                <a href='/portfolio/android-fragments'> Android Fragments </a> 
+                            </div>
+                        </li>
+                        
+                    </ul>
+                </div>
+
+
+                <div class="content">
+                    @yield('content')
+                </div>
+
+
+        </div>
     </div>
+
+
 
 
 </body>
